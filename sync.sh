801ebb68dbee50d22f37bc5f154ad5b83e64d287
JayShore <letternumber10@gmail.com>
2016-04-06 14:50:47 -0700

    Automated upload by script

--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-NOW=$(date +"%m-%d-%y")
-cd /var/www/application #cd to right place
-git checkout -b AutoUpload_$NOW #create the new branch
-git add ./* #add any updates 
-git commit -a -m "Automated upload by script" #commit with a message
-git push -u origin AutoUpload_$NOW #upload the changes
-
